cb544007b05729eb10fa98ad0ee0c8371194780f
Alex Frederiksen <ajfred154@gmail.com>
2017-11-13 21:28:25 -0500

    Add documentation to controller. Need Fix.

--- led_controller.cpp
+++ led_controller.cpp
@@ -20,6 +20,7 @@ void SnakeController::update() {
 	colors::color_rgb_t rgb;
 	float interval = 360.0 / (float) length;
 	for (int i = 0; i < length; i++) {
+		// convert that hsv color to a cooler rgb color
 		colors::convert_HSV2RGB(interval * i, 1.0f, 1.0f, rgb);
 		leds.set_led(((int) head_pos - i + leds.count) % leds.count, 
 			(int) (rgb.r * 255), 
